e3d5ad009cade8ef8e60b71ee45f283836fb38fb
gopi19 <130206975+gopi19@users.noreply.github.com>
2025-12-15 18:36:03 +0530

    Update RegisterRequest.java
    for valid registration

--- budget-backend/src/main/java/com/gopi/budgetwise/dto/RegisterRequest.java
+++ budget-backend/src/main/java/com/gopi/budgetwise/dto/RegisterRequest.java
@@ -4,19 +4,29 @@ import jakarta.validation.constraints.*;
 
 public class RegisterRequest {
 
-    @NotBlank
+    @NotBlank(message = "Name is required")
     public String name;
 
-    @Email
-    @NotBlank
+
+    @NotBlank(message = "Email is required")
+    //@Email(message = "Enter a valid email address (example@domain.com)")
+    @Pattern(
+            regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$",
+            message = "Enter a valid email address (example@domain.com)"
+    )
     public String email;
 
-    @NotBlank
-    @Size(min = 4)
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, message = "Password must be at least 6 characters long")
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).+$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, and one number"
+    )
     public String password;
 
     // profile
     public Double monthlyIncome;
     public Double monthlyTargetExpenses;
     public Double savingsTarget;
+
 }
